5f04cd4ee0edfc7c78ffed66e6a8a4f8cf05a0ec
Stephen Moore <delfick755@gmail.com>
2010-07-11 13:39:27 +0800

    moving views tests into its own folder

---
+++ views/tests/test_view2.py
@@ -0,0 +1,14 @@
+# coding: spec
+
+describe 'Getting result from a normal view':
+    it 'needs tests'
+
+describe 'Getting result from a StaffOnly view':
+    it 'needs tests'
+    
+describe 'Getting result from a LocalOnly view':
+    it 'needs tests'
+    
+describe 'Getting result from a JSView view':
+    it 'needs tests'
+
