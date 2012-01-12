086314f4aa7fb86dde7c2a3667504ab42c16f0ce
Stephen Moore <stephen@delfick.com>
2012-01-12 18:54:16 +1100

    Making ButtonsAdmin use django 1.4 features

--- admin/buttons.py
+++ admin/buttons.py
@@ -18,7 +18,8 @@ import rendering
 class Button(object):
     def __init__(self
         , url, desc
-        , kls=None, saveOnClick=True, forAll=False, display=True, needSuperUser=True):
+        , kls=None, saveOnClick=True, forAll=False, display=True, needSuperUser=True
+        , executeAndRedirect=False):
         
         if forAll:
             saveOnClick = False
@@ -29,6 +30,7 @@ class Button(object):
         self.display = display
         self.saveOnClick = saveOnClick
         self.needSuperUser = needSuperUser
+        self.executeAndRedirect = executeAndRedirect
         
     def __unicode__(self):
         if not self.saveOnClick or self.forAll:
@@ -94,8 +96,8 @@ class ButtonAdminMixin(object):
         """Action taken when a button is pressed"""
         model = self.model
         obj = get_object_or_404(model, pk=object_id)
-        result = getattr(self, "tool_%s" % button.url)(request, obj, button)
-        
+        result = self.getResultForButton(request, obj, button)
+    
         try:
             File, extra = result
         except:
@@ -113,6 +115,18 @@ class ButtonAdminMixin(object):
         }
         context.update(extra or {})
         return rendering.render(request, File, context)
+        
+    def getResultForButton(self, request, obj, button):
+        name = "tool_%s" % button.url
+        func = getattr(self, name, None)
+        if not func and button.executeAndRedirect:
+            def func(request, obj, button):
+                getattr(obj, button.executeAndRedirect)()
+                url = '/admin/%s/%s/%s' % (obj._meta.app_label, obj._meta.module_name, obj.id)
+                return rendering.redirect(url)
+            func.__name__ = name
+            
+        return func(request, obj, button)
 
 ########################
 ###   BUTTON ADMIN
@@ -131,98 +145,30 @@ class ButtonAdmin(admin.ModelAdmin, ButtonAdminMixin):
         else:
             return self.get_urls()
     
-    def changelist_view(self, request, extra_context=None):
+    def add_buttons(self, request, context):
+        if hasattr(self, 'buttons') and self.buttons:
+            [b.setShow(request.user) for b in self.buttons]
+            context['buttons'] = self.buttons
+    
+    def changelist_view(self, request, *args, **kwargs):
         """Add buttons to changelist view"""
-        if not extra_context:
-            extra_context = {}
-        
-        if hasattr(self, 'buttons') and self.buttons:
-            [b.setShow(request.user) for b in self.buttons]
-            extra_context['buttons'] = self.buttons
-        
-        return super(ButtonAdmin, self).changelist_view(request, extra_context)
-        
-    def change_view(self, request, object_id, extra_context=None):
+        response = super(ButtonAdmin, self).changelist_view(request, *args, **kwargs)
+        self.add_buttons(request, response.context_data)
+        return response
+    
+    def render_change_form(self, request, *args, **kwargs):
         """Add buttons to change view"""
-        if not extra_context:
-            extra_context = {}
-        
-        if hasattr(self, 'buttons') and self.buttons:
-            [b.setShow(request.user) for b in self.buttons]
-            extra_context['buttons'] = self.buttons
-        
-        result = super(ButtonAdmin, self).change_view(request, object_id, extra_context)
-        
+        response = super(ButtonAdmin, self).render_change_form(request, *args, **kwargs)
+        self.add_buttons(request, response.context_data)
+        return response
+    
+    def response_change(self, request, obj):
         redirect = None
         for key in request.POST.keys():
             if key.startswith("tool_"):
                 redirect = key
-                
-        if not redirect:
-            return result
-        else:
-            # A lot of this is copied from django.contrib.admin.ModelAdmin.change_view
-            model = self.model
-            opts = model._meta
-
-            try:
-                obj = self.queryset(request).get(pk=unquote(object_id))
-            except model.DoesNotExist:
-                # Don't raise Http404 just yet, because we haven't checked
-                # permissions yet. We don't want an unauthenticated user to be able
-                # to determine whether a given object exists.
-                obj = None
-
-            if not self.has_change_permission(request, obj):
-                raise PermissionDenied
-
-            if obj is None:
-                raise Http404(
-                    _('%(name)s object with primary key %(key)r does not exist.') % {
-                          'name': force_unicode(opts.verbose_name)
-                        , 'key': escape(object_id)
-                        }
-                    )
-
-            if request.method == 'POST' and request.POST.has_key("_saveasnew"):
-                return self.add_view(request, form_url='../add/')
-
-            ModelForm = self.get_form(request, obj)
-            formsets = []
-            if request.method == 'POST':
-                form = ModelForm(request.POST, request.FILES, instance=obj)
-                if form.is_valid():
-                    form_validated = True
-                    new_object = self.save_form(request, form, change=True)
-                else:
-                    form_validated = False
-                    new_object = obj
-                prefixes = {}
-                for FormSet, inline in zip(
-                    self.get_formsets(request, new_object), self.inline_instances):
-                    prefix = FormSet.get_default_prefix()
-                    prefixes[prefix] = prefixes.get(prefix, 0) + 1
-                    if prefixes[prefix] != 1:
-                        prefix = "%s-%s" % (prefix, prefixes[prefix])
-                    
-                    formset = FormSet(
-                          request.POST, request.FILES, instance=new_object, prefix=prefix
-                        , queryset=inline.queryset(request)
-                        )
-                    
-                    formsets.append(formset)
-                
-                if all_valid(formsets) and form_validated:
-                    self.save_model(request, new_object, form, change=True)
-                    form.save_m2m()
-                    for formset in formsets:
-                        self.save_formset(request, form, formset, change=True)
-                    
-                    change_message = self.construct_change_message(request, form, formsets)
-                    self.log_change(request, new_object, change_message)
-                    
-                    # Redirect instead of return normal response
-                    return rendering.redirect(redirect)
         
-        # Wasn't a POST or errors when saved
-        return result
+        if redirect:
+            return rendering.redirect(redirect)
+        else:
+            return super(ButtonAdmin, self).response_change(request, obj)
