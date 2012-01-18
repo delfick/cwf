eb36617ed0c3099619e0d7dd5ba28ed6776e5595
Stephen Moore <stephen@delfick.com>
2012-01-18 22:21:43 +1100

    updating manage.py

--- helpers/manage.py
+++ helpers/manage.py
@@ -12,18 +12,9 @@ def manager(location, getPaths=None):
     project = location.split(os.sep)[-1]
     os.environ['DJANGO_SETTINGS_MODULE'] = '%s.settings' % project
     
-    from django.core.management import execute_manager
     if getPaths:
         sys.path = getPaths(project, ignoreMissing=True) + sys.path
-    # Import the site so that any funky loading can happen in __init__.py
-    # For example, making django think there is a settings.py when there isn't.
-    __import__(project, globals(), locals(), [], -1)
-    try:
-        import settings # Assumed to be in the same directory.
-    except ImportError:
-        import sys
-        sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
-        sys.exit(1)
     
-    execute_manager(settings)
-        
+    from django.core.management import execute_from_command_line
+    
+    execute_from_command_line(sys.argv)
