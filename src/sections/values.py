5a60877ab376f2c471d50a91f3e097db34924b6b
Stephen Moore <stephen@delfick.com>
2012-04-24 22:16:08 +1000

    Some adjustments to Values class

--- src/sections/values.py
+++ src/sections/values.py
@@ -7,13 +7,13 @@ class Values(object):
             values = []
         
         # each: Transform each value in values individually into alias, url_part
-        #   Must be callble((request, parent_url_parts, path), value)->(alias, url_part)
+        #   Must be callable((request, path, parent_url_parts), value)->(alias, url_part)
         self.each = each
         if not callable(self.each):
             raise ConfigurationError("each must be a callable, not %s" % self.each)
         
         # values: The values to use
-        #   Can be list or callable((request, parent_url_parts, path))->[]
+        #   Can be list or callable((request, path, parent_url_parts))->[]
         self.values = values
         
         # sorter: Function to sort values
@@ -30,43 +30,62 @@ class Values(object):
         # sort_after_transform: Determine if sorting happens after self.each is used
         self.sort_after_transform = sort_after_transform
     
-    def get_info(self, request, path, parent_url):
+    def get_info(self, request, path, parent_url_parts):
         """Yield (alias, url) for each value"""
         # Get sorted values
-        values = self.getValues(request, parentUrl, path)
+        info = (request, path, parent_url_parts)
+        values = self.get_values(info)
             
         # Yield some information
         if values and any(v is not None for v in values):
             for alias, url in values:
                 yield alias, url
     
-    def get_values(self, request, parent_url_parts, path):
+    def get_values(self, info):
         """Get transformed, sorted values"""
         if not self.values:
             return None
         
-        # Get a list of values
-        values = self.values
-        if callable(values):
-            values = list(values((request, parent_url_parts, path)))
-        
-        # Remove duplicates
-        if self.as_set:
-            values = set(values)
+        values = normalised_values(info)
         
         # Sort if we have to
         if not self.sort_after_transform:
             values = self.sort(values)
         
         # Tranform as appropiate
-        if self.each:
-            values = list(self.each((request, parent_url_parts, path), value))
-        else:
-            values = [(value, value) for value in values]
+        transformed = self.transform_values(values, info)
             
         # Sort if we haven't yet
         if self.sort_after_transform:
-            values = self.sort(values)
+            transformed = self.sort(transformed)
+        
+        return transformed
+    
+    def transform_values(self, values, info):
+        ''''
+            use self.each on values if self.each is defined
+            self.each will be called as self.each(info, value) for each value in values
+            Otherwise turn values list of [v1, v2, v3] into [(v1, v1), (v2, v2), (v3, v3)]
+        '''
+        if self.each:
+            return list(self.each(info, value) for value in values)
+        else:
+            return [(value, value) for value in values]
+    
+    def normalised_values(self, info):
+        '''
+            Return values as a list
+            If values is a callable, get the result of calling it with info
+            If we want values as set, then do that here
+        '''
+        # Get a list of values
+        values = self.values
+        if callable(values):
+            values = list(values(info))
+        
+        # Remove duplicates
+        if self.as_set:
+            values = set(values)
         
         return values
     
