ccc83972c169aae521e961767408b4a19f972d0c
Stephen Moore <stephen@delfick.com>
2012-07-14 21:05:42 +1000

    Adding init helper to src.sections and shortcuts to Section, Options and Values

---
+++ src/sections/__init__.py
@@ -0,0 +1,22 @@
+from section import Section
+from options import Options
+from values import Values
+
+S = Section
+O = Options
+V = Values
+
+def init(package, name=None, kls='views'):
+    """
+        Helper to create a Section object with defaults for module and kls
+        Use like section = init(__package__)
+    """
+    if not name:
+        name = package.split('.')[-1]
+    alias = name.capitalize()
+
+    return S('', name, None).configure(''
+         , kls = kls
+         , alias = alias
+         , module = '%s.views' % package
+         )
