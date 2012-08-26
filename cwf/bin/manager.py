80d06bc8a223a5a9f8af2c94ecf4c8b23b68f228
Stephen Moore <stephen@delfick.com>
2012-08-26 21:47:56 +1000

    Making manage script a python file instead of bash script overriding django-manage.sh

---
+++ cwf/bin/manager.py
@@ -0,0 +1,19 @@
+#!/usr/bin/evn python
+
+from cwf.splitter.manager import manager
+import os, sys
+
+def main():
+    if 'WEBLIBS' in os.environ:
+        sys.path.insert(0, os.environ['WEBLIBS'])
+
+    get_path = None
+    try:
+        from wsgibase import get_path
+    except ImportError:
+        pass
+
+    manager(os.getcwd(), get_path)
+
+if __name__ == '__main__':
+    main()
