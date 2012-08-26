a097fededc68d6ede3ee573d405cf05c04a73640
Stephen Moore <stephen@delfick.com>
2012-08-26 16:08:16 +1000

    Making django-manage.sh not depend on wsgibase.get_path

--- bin/django-manage.sh
+++ bin/django-manage.sh
@@ -2,11 +2,15 @@
 # Put this in your bash path and you don't need a manage.py file
 # Will add os.environ['WEBLIBS'] to the start of your sys.path
 # Then assume cwf is in your path
-# And that 'from wsgibase import get_path' will work
+# If it can do 'from wsgibase import get_path' then get_path is passed into manager
+# The manager will use this function to determine what it prepends sys.path with
 python -c "
 import os, sys
 if 'WEBLIBS' in os.environ:
     sys.path.insert(0, os.environ['WEBLIBS'])
 from cwf.splitter.manager import manager
-from wsgibase import get_path
+try:
+    from wsgibase import get_path
+except ImportError:
+    get_path = None
 manager(os.getcwd(), get_path)" $@
