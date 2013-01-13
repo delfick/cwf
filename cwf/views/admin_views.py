180b5ea5b4c830f8a9c147ad0e9e406115cb11e2
Stephen Moore <stephen@delfick.com>
2013-01-13 12:59:29 +1100

    AdminView documentation

--- cwf/views/admin_views.py
+++ cwf/views/admin_views.py
@@ -2,25 +2,28 @@ from django.contrib.contenttypes.models import ContentType
 from django.core import urlresolvers
 
 class AdminView(object):
-    """Object that knows how to get url to an admin view for an object"""
+    """Stateless Object that knows how to get url to an admin view"""
     @classmethod
     def format(cls, content_type, page):
         return "admin:%s_%s_%s" % (content_type.app_label, content_type.model, page)
 
     @classmethod
     def change_view(cls, obj):
+        """Admin change view for a particular instance of an object"""
         content_type = ContentType.objects.get_for_model(obj.__class__)
         format = cls.format(content_type, "change")
         return urlresolvers.reverse(format, args=(obj.id, ))
 
     @classmethod
     def add_view(cls, obj):
+        """Admin add view for a particular model"""
         content_type = ContentType.objects.get_for_model(obj)
         format = cls.format(content_type, "add")
         return urlresolvers.reverse(format)
 
     @classmethod
     def change_list(cls, obj):
+        """Admin list view for a particular model"""
         content_type = ContentType.objects.get_for_model(obj)
         format = cls.format(content_type, "changelist")
         return urlresolvers.reverse(format)
--- docs/docs/views/admin_views.rst
+++ docs/docs/views/admin_views.rst
@@ -2,3 +2,11 @@
 
 Admin Views Urls
 ================
+
+Getting the url to a view in the admin section requires using ContentType and
+urlresolvers and is less than concise.
+
+So, to assist with creating these urls, CWF provides ``AdminView``.
+
+.. autoclass:: cwf.views.admin_views.AdminView
+    :members:
